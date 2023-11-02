ddcae5e28b83126db1c65da1e09897ee2bd11a2b
Sameh ElGazar <selgazar95@gmail.com>
2023-11-03 00:57:40 +0200

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,8 @@
 if __name__ == "__main__":
     import sys
 
-    length = len(sys.argv) -1
+    length = len(sys.argv) - 1
+
     if length == 0:
         print("0 arguments.")
     elif length == 1:
