2c1c7813ebada1274565231b5a75ac631a47352d
Sameh ElGazar <selgazar95@gmail.com>
2023-11-03 00:37:05 +0200

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
-args = sys.argv
-argsLen = len(args)
+if __name__ == "__main__":
+    import sys
+    args = sys.argv
+    argsLen = len(args)
 
-if argsLen == 1:
-    print("0 arguments.")
-elif argsLen == 2:
-    print('''1 argument:
-1: {}'''.format(args[1]))
-else:
-    print("{} arguments:". format(argsLen))
-    for i in range(1, argsLen):
-        print("{}: {}".format(i, args[i]))
+    if argsLen == 1:
+        print("0 arguments.")
+    elif argsLen == 2:
+        print('''1 argument:
+    1: {}'''.format(args[1]))
+    else:
+        print("{} arguments:". format(argsLen))
+        for i in range(1, argsLen):
+            print("{}: {}".format(i, args[i]))
