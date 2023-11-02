fa89d10867178a3e22cdcf84c78e2e4eef63f1d7
Sameh ElGazar <selgazar95@gmail.com>
2023-11-02 23:49:26 +0200

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,5 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(str(a), str(b), str(add(a, b))))
+    sum = add(a, b)
+    print("{} + {} = {}".format(str(a), str(b), str(sum)))
