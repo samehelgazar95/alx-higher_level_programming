a9c3e63b3eb2c4697bc89a4e4e004fb5b3946b1c
Sameh ElGazar <selgazar95@gmail.com>
2023-11-04 14:55:50 +0200

    Name == main

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for ele in my_list:
-        print("{}".format(ele))
+if __name__ == "__main__":
+    def print_list_integer(my_list=[]):
+        for ele in my_list:
+            print("{}".format(ele))
