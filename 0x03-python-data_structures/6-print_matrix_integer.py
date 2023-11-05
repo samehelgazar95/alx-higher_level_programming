4d2de04d3d06a1845af5367e5beb8bb0245cc512
Sameh ElGazar <selgazar95@gmail.com>
2023-11-05 03:40:44 +0200

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+
     for row in matrix:
+        if (len(row) == 0):
+            print()
         for ele in row:
-            print("{:d}".format(ele), end=' ')
-        print()
+            print("{:d}".format(ele),
+                  end='\n' if ele == row[len(row) - 1] else ' ')
