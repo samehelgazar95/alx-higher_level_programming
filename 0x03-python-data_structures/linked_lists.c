fa7cf70f403cf14e4d6e998e8708f132f5ddea09
Sameh ElGazar <selgazar95@gmail.com>
2023-11-07 13:25:07 +0200

    Delete at

--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -10,21 +10,20 @@
 
 size_t print_listint(const listint_t *h)
 {
-const listint_t *current;
-unsigned int n; /* number of nodes */
+    const listint_t *current;
+    unsigned int n; /* number of nodes */
 
-current = h;
-n = 0;
-while (current != NULL)
-{
-printf("%i\n", current->n);
-current = current->next;
-n++;
+    current = h;
+    n = 0;
+    while (current != NULL)
+    {
+        printf("%i\n", current->n);
+        current = current->next;
+        n++;
+    }
+
+    return (n);
 }
-
-return (n);
-}
-
 /**
  * add_nodeint_end - adds a new node at the end of a listint_t list
  * @head: pointer to pointer of first node of listint_t list
