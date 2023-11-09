a26de16d80dca307b41feadc95499f9d95f22717
Sameh ElGazar <selgazar95@gmail.com>
2023-11-09 03:30:48 +0200

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+
+    score_key = None
+    score_val = 0
+    for key in a_dictionary.keys():
+        if a_dictionary[key] > score_val:
+            score_val = a_dictionary[key]
+            score_key = key
+
+    return score_key
