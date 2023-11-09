367392ae3058e591476cea70880042fd98a828df
Sameh ElGazar <selgazar95@gmail.com>
2023-11-09 04:54:32 +0200

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+
+    res = 0
+    roman_string = roman_string.upper()
+    d = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+        }
+
+    for i in range(len(roman_string)):
+        if i < len(roman_string) - 1 and \
+                d[roman_string[i]] < d[roman_string[i + 1]]:
+            res -= d[roman_string[i]]
+        else:
+            res += d[roman_string[i]]
+
+    return res
