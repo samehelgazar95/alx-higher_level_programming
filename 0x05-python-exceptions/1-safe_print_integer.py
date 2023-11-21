2d6fab8baebb2d83cab5a11858ecb227521e0114
Sameh ElGazar <selgazar95@gmail.com>
2023-11-21 02:27:22 +0200

    Task 100

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        # raise TypeError(value, "is not an integer")
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError):
