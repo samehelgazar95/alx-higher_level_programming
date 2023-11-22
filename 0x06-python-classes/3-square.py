19e159bcabb4d8553b7b5376704f54c1a17ef810
Sameh ElGazar <selgazar95@gmail.com>
2023-11-23 01:51:11 +0200

    Task 2.1

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Square attribute."""
+
+
+class Square:
+    """ Class For Square
+    Attributes:
+        size: the size of square
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
