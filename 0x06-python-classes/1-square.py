903ea2c993ec01db4d0caad3ec19812ceedcdfa7
Sameh ElGazar <selgazar95@gmail.com>
2023-11-23 00:40:10 +0200

    Task 2.0

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Square attribute."""
+
+
+class Square:
+    """ Class For Square
+    Attributes:
+        size: the size of square
+    """
+    def __init__(self, size):
+        self.__size = size
