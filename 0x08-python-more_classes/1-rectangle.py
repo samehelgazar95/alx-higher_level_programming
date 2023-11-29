b61212f16302a5cebe7e34ce59cba0df0cd2ee03
Sameh ElGazar <selgazar95@gmail.com>
2023-11-29 16:03:25 +0200

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,29 +6,34 @@ class Rectangle():
     """ Simple Rectangle Class """
 
     def __init__(self, width=0, height=0):
+        """The Instance"""
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
+        """Width Getter"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Width Setter"""
         if type(value) != int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
+        """Heigth Getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Heigth Setter"""
         if type(value) != int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
