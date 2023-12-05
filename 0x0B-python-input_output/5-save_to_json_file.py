8d7de0fac57e5080b6b46332fb7cfbd092983c53
Sameh ElGazar <selgazar95@gmail.com>
2023-12-06 00:05:11 +0200

    Task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""writes an Object to a text file module"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file func"""
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obj, sort_keys=True))
