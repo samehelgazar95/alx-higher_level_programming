f05c544726745e5038fcba3cdc75f8d869d053ac
Sameh ElGazar <selgazar95@gmail.com>
2023-12-05 22:32:44 +0200

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Write to file Module"""
+
+
+def write_file(filename="", text=""):
+    """Write to file Function"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        return (f.write(text))
