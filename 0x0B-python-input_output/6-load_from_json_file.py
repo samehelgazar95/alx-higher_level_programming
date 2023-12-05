ceb34baea3f97803ed925c8cc93036ebfab5a5e0
Sameh ElGazar <selgazar95@gmail.com>
2023-12-06 00:12:40 +0200

    Task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""writes an Object to a text file module"""
+import json
+
+
+def load_from_json_file(filename):
+    """writes an Object to a text file func"""
+    with open(filename, 'r') as f:
+        return json.loads(f.read())
