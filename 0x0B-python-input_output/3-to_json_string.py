2cd7a9b410167d082d68fa790922cb7c7571c98a
Sameh ElGazar <selgazar95@gmail.com>
2023-12-05 22:55:06 +0200

    Task 3.1

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import json
+"""To json string using json.dumps module"""
 
 
 def to_json_string(my_obj):
+    """To json string using json.dumps function"""
     return json.dumps(my_obj)
