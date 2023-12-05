2c7d12d4afb98b4537d20b19cb876a7d4509f9c4
Sameh ElGazar <selgazar95@gmail.com>
2023-12-06 00:55:48 +0200

    Task 7.1

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 """load_add_save to a text file module"""
-import json
 import sys
+import os
 
 
 def load_add_save():
     """load_add_save to a text file func"""
 
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    save_ = __import__('5-save_to_json_file').save_to_json_file
+    load_ = __import__('6-load_from_json_file').load_from_json_file
 
     my_list = []
+
+    if os.path.exists("add_item.json"):
+        my_list.extend(load_("add_item.json"))
+
     for i in range(1, len(sys.argv)):
         my_list.append(sys.argv[i])
 
-    print(my_list)
-    save_to_json_file(my_list, "add_item.json")
+    save_(my_list, "add_item.json")
 
 
 if __name__ == '__main__':
