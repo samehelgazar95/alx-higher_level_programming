6fca91b3b1715d065c8bcfc24afa1d3051cb5d3b
Sameh ElGazar <selgazar95@gmail.com>
2023-12-05 22:59:55 +0200

    Task 4

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""To json string using json.dumps module"""
+"""convert to json string using json.dumps module"""
 import json
 
 
 def to_json_string(my_obj):
-    """To json string using json.dumps function"""
+    """convert to json string using json.dumps function"""
     return json.dumps(my_obj)
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""convert to obj using json.loads module"""
+import json
+
+
+def from_json_string(my_str):
+    """convert to obj using json.loads func"""
+    return json.loads(my_str)
