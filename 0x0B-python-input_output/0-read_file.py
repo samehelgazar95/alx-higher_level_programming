4a813329e2131bca0ce98f162878ff6409a90d47
Sameh ElGazar <selgazar95@gmail.com>
2023-12-05 22:18:49 +0200

    Task 0.3

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,7 +4,7 @@
 
 def read_file(filename=""):
     """Read File Function"""
-    with open(filename) as f:
+    with open(filename, 'r', encoding='UTF-8') as f:
         content = f.read()
 
         if content[-1] == '\n':
