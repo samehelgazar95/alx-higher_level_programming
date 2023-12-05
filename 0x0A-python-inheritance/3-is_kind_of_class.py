1c073295fe9a39e015180a334835ce8fc4fb1693
Sameh ElGazar <selgazar95@gmail.com>
2023-12-05 03:11:22 +0200

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Simple isinstance module"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Simple isinstance function
+
+    @obj: the object
+    @a_class: the class
+    """
+    return isinstance(obj, a_class)
