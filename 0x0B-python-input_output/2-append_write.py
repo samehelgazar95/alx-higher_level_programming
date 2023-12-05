93a1bbb74b5aa190401192b7a8f2ff8ed8c663ea
Sameh ElGazar <selgazar95@gmail.com>
2023-12-05 22:35:51 +0200

    Task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Append to file Module"""
+
+
+def append_write(filename="", text=""):
+    """Append to file Function"""
+    with open(filename, 'a', encoding='utf-8') as f:
+        return (f.write(text))
