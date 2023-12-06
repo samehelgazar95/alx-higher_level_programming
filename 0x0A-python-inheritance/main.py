9588024b06c89fe7697946fd336e36328d690668
Sameh ElGazar <selgazar95@gmail.com>
2023-12-06 18:16:01 +0200

    Task 7.5

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -3,9 +3,9 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
 
-bg.integer_validator("my_int", -2147483642147483647789)
+bg.integer_validator("my_int", 219)
 
 try:
-    bg.integer_validator("long", 2147483647)
+    bg.integer_validator("intStr", "123")
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
