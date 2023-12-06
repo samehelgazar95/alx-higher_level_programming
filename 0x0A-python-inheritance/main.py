801421248ce92e595c747992949aa486d13d9f7b
Sameh ElGazar <selgazar95@gmail.com>
2023-12-07 00:45:42 +0200

    Task 7.11

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -3,9 +3,8 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
 
-# bg.integer_validator(None, 2)
-# bg.integer_validator("", 2)
-bg.integer_validator("Hello", (1, 2))
+
+bg.integer_validator()
 
 
 
