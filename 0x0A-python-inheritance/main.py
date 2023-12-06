dc6f1462e0135f58d12dbe4a936836dc6db354de
Sameh ElGazar <selgazar95@gmail.com>
2023-12-06 18:22:50 +0200

    Task 7.7

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -6,6 +6,6 @@ bg = BaseGeometry()
 bg.integer_validator("my_int", 219)
 
 try:
-    bg.integer_validator("intStr", "123")
+    bg.integer_validator("float", 5-2.5)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
