9b996007c5573df97bd59d212a59a3cf800320c4
Sameh ElGazar <selgazar95@gmail.com>
2023-12-06 03:21:32 +0200

    Task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Class Student module"""
+
+
+class Student:
+    """Class Student func"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            dict = {}
+            for attr in attrs:
+                value = getattr(self, attr, "Error")
+                if value == "Error":
+                    continue
+                dict[attr] = value
+            return dict
