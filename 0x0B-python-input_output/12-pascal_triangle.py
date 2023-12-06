8f8a48c839aa631410747eee284c7fde8d73da3c
Sameh ElGazar <selgazar95@gmail.com>
2023-12-06 15:47:40 +0200

    all

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+""" Pascal Triangle Module
+"""
+
+# n = 5
+# [[1], [1,1], [1,2,1], [1,3,3,1], [1,4,6,4,1]]
+
+def pascal_triangle(n):
+    """ Pascal Triangle Func
+    """
+    
+    pascal = []
+    
+    if (n <= 0):
+        return pascal
+    
+    for i in range(n):
+        pascal.append([1 * (i + 1)])
+        
+    print(pascal)
+    
+
+    
+pascal_triangle(5)
