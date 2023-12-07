fff19caa896ca74bcd26b2ec2e2fa3b55d5ea146
Sameh ElGazar <selgazar95@gmail.com>
2023-12-07 02:37:52 +0200

    Task 7.507

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Hello, iam BaseGeometry module"""
+"""BaseGeometry module"""
 
 
 class BaseGeometry:
-    """Hello, iam BaseGeometry Class"""
+    """BaseGeometry Class"""
 
     def area(self):
-        """Hello, iam area method"""
+        """area method"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Hello, iam Validating Int Method"""
+        """Validating Int Method"""
         if (type(value) != int):
             raise TypeError("{} must be an integer".format(name))
         if (value <= 0):
