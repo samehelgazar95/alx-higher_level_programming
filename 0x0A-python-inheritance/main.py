fa3162cf905a9070944c4339f5ebba77c840391f
Sameh ElGazar <selgazar95@gmail.com>
2023-12-07 02:57:21 +0200

    Task 7 final

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -2,9 +2,9 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 gModule = __import__("7-base_geometry").__doc__
-gClass = __import__("7-base_geometry").BaseGeometry.__doc__
-gArea = __import__("7-base_geometry").BaseGeometry.area.__doc__
-gInt = __import__("7-base_geometry").BaseGeometry.integer_validator.__doc__
+gClass = BaseGeometry.__doc__
+gArea = BaseGeometry.area.__doc__
+gInt = BaseGeometry.integer_validator.__doc__
 
 print(gModule)
 print(gClass)
