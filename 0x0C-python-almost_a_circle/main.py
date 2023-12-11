0bd37b5eb5e280b0cc3f671e295ec40cd3d57e64
Sameh ElGazar <selgazar95@gmail.com>
2023-12-11 23:55:36 +0200

    Task 15

--- 0x0C-python-almost_a_circle/main.py
+++ 0x0C-python-almost_a_circle/main.py
@@ -9,16 +9,14 @@ from models.square import Square
 
 
 if __name__ == "__main__":
-
-    r1 = Rectangle(10, 2)
-    print(r1.id)
-
-    r2 = Rectangle(2, 10)
-    print(r2.id)
-
-    r3 = Rectangle(10, 2, 0, 0, 12)
-    print(r3.id)
     
+    r1 = Rectangle(10, 7, 2, 8)
+    dictionary = r1.to_dictionary()
+    json_dictionary = Base.to_json_string([dictionary])
+    print(dictionary)
+    print(type(dictionary))
+    print(json_dictionary)
+    print(type(json_dictionary))
     
     print(Base.__doc__)
     print(models.base.__doc__)
