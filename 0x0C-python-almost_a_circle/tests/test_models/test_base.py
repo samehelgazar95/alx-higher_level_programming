7da5ffa5a1e151eba968a33a2d8906f3b66584c4
Sameh ElGazar <selgazar95@gmail.com>
2023-12-12 16:51:13 +0200

    Tesssts

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """UnitTest for Base Class Module"""
 import unittest
+import models
 from models.base import Base
-import models.base
 
 
 class TestBase(unittest.TestCase):
@@ -19,39 +19,48 @@ class TestBase(unittest.TestCase):
         self.assertTrue(len(Base.__doc__) > 0)
         
     def test_doc_class(self):
+        """ test doc class """
         self.assertTrue(len(models.base.__doc__) > 0)
     
-    def test_is__Base_class(self):
+    def test_is_Base_class(self):
+        """ test is Base class """
         b = Base()
         self.assertTrue(str(b.__class__), "<class 'models.base.Base'>")
 
     def test_class_has_id(self):
+        """ test class has id """
         b = Base()
         self.assertTrue(hasattr(b, "id"))
 
     def test_class_has_id(self):
+        """ test class has id """
         b = Base()
         self.assertTrue(hasattr(b, "_Base__nb_objects"))
 
     def test_no_id(self):
+        """ test no id """
         b = Base()
         self.assertEqual(getattr(b, "_Base__nb_objects"), b.id)
 
     def test_zero_id(self):
+        """ test zero id """
         b = Base(0)
         self.assertEqual(b.id, 0)
 
     def test_two_empty_instances(self):
+        """ test two empty instances """
         b = Base()
         bb = Base()
         self.assertEqual(b.id + 1, bb.id)
 
     def test_two_instances(self):
+        """ test two instances """
         b = Base(5)
         b = Base()
         self.assertEqual(b.id, 1)
 
     def test_positive_id(self):
+        """ test positive id """
         b = Base(13)
         self.assertEqual(b.id, 13)
 
