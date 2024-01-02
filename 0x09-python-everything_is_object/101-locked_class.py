4e1599625960950e1e5714192abbc61c27c3a431
Sameh ElGazar <selgazar95@gmail.com>
2024-01-02 14:23:42 -0800

    Task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,7 +2,22 @@
 
 
 class LockedClass:
+     """
+        A class with restricted attribute creation using __slots__.
+
+        Attributes:
+        __slots__ (tuple): A tuple containing the allowed attribute names.
+        first_name (str): The first name of an instance.
+
+        Methods:
+        __init__(): Initializes an instance of the LockedClass.
+    """
+    
     __slots__ = ('first_name',)
     
     def __init__(self):
+        """
+            Initializes an instance of the LockedClass with a default value for first_name.
+        """
+
         self.first_name = None
