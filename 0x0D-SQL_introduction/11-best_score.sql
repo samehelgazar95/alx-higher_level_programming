0e5c0d5b25e58838b5af6ad105c147ab54e63203
Sameh ElGazar <selgazar95@gmail.com>
2024-01-17 06:00:20 +0200

    Fix 1

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -4,7 +4,7 @@
 
 -- Display both the score and the name (in this order)
 -- With "score" >= 10, Ordered by score (top first)
-SELECT "score", "name"
+SELECT score, name
 FROM second_table
 WHERE score >= 10
 ORDER BY score DESC;
