dd2816008fcba3542ae95f75f107ca6de1e70a12
Sameh ElGazar <selgazar95@gmail.com>
2024-01-19 01:21:21 +0200

    Task1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,1 +1,8 @@
--- 
+-- Create user with password & all PRIVILEGES
+
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* To 'user_0d_1'@'localhost'
+WITH GRANT OPTION;
+FLUSH PRIVILEGES;
