6c954b1f33cad9cdf46dbfb730dcce75349c63a9
Sameh ElGazar <selgazar95@gmail.com>
2024-01-19 01:37:30 +0200

    Task2.1

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
 -- Creating the database hbtn_0d_2 and the user user_0d_2.
+
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 CREATE USER IF NOT EXISTS 'user_0d_2'
-IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT on 'hbtn_0d_2' To 'user_0d_2';
+IDENTIFIED BY 'user_0d_2_pwd'@'localhost';
+GRANT SELECT on 'hbtn_0d_2' To 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
