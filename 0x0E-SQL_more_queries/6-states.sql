0a955ab69f994e2f066054683d62fefc2ab52f9f
Sameh ElGazar <selgazar95@gmail.com>
2024-01-19 04:09:42 +0200

    Task6.1

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,8 @@
 -- Creates the database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+
+USE `hbtn_0d_usa`;
+
 CREATE TABLE IF NOT EXISTS `states` (
     id INT NOT NULL UNIQUE PRIMARY KEY,
     name VARCHAR(256)
