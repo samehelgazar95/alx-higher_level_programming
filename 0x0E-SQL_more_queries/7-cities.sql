38a3b9530b2cf6c932e3db2a0a842ea4b4e140ea
Sameh ElGazar <selgazar95@gmail.com>
2024-01-19 04:27:59 +0200

    Task7.1

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,10 @@
 -- Creates the database hbtn_0d_usa and the table cities
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-USE `hbtn_0d_usa`;
+USE hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS `cities` (
+CREATE TABLE IF NOT EXISTS cities (
     id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-    state_id INT NOT NULL FOREIGN KEY(id),
+    state_id INT NOT NULL FOREIGN KEY(id) REFERENCES cities(id),
     name VARCHAR(256) NOT NULL
 );
