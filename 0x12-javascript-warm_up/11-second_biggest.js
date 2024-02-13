d90ada54f8ded5153ad5458607d64dd9bb681862
Sameh ElGazar <selgazar95@gmail.com>
2024-02-13 04:18:42 +0200

    Tasks

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,6 +5,6 @@ const args = process.argv;
 if (args.length <= 3) {
   console.log(0);
 } else {
-  args.slice(3).sort();
-  console.log(args[2]);
+  const newArgs = args.slice(2).sort((a, b) => b - a);
+  console.log(newArgs[1]);
 }
