e42f7423713be299d0d231257baad78a52b1810a
Sameh ElGazar <selgazar95@gmail.com>
2024-02-13 03:55:55 +0200

    Tasks

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,2 +1,15 @@
 #!/usr/bin/node
 
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n === 1) {
+    return 1;
+  }
+
+  return factorial(n - 1) * n;
+}
+
+const num = Number(process.argv[2]);
+console.log(factorial(num));
