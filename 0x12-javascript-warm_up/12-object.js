9cba6a74fa73dd1776da1859ad5e99479a138c82
Sameh ElGazar <selgazar95@gmail.com>
2024-02-13 04:20:26 +0200

    Tasks

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
