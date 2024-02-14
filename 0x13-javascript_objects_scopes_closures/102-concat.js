643fcb26cfcf942e2c300806447e99d9de2c2edb
Sameh ElGazar <selgazar95@gmail.com>
2024-02-14 16:05:12 +0200

    Advanced

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
 
-const fs = require('node:fs');
+function concatsFiles(src1, src2, dest) {
+  // Sync
+  const fs = require('node:fs');
+  const fileA = fs.readFileSync(src1, 'utf8');
+  const fileB = fs.readFileSync(src2, 'utf8');
 
-// Sync
-const fileA = fs.readFileSync('fileA', 'utf8');
-const fileB = fs.readFileSync('fileB', 'utf8');
+  fs.appendFileSync(dest, fileA);
+  fs.appendFileSync(dest, fileB);
+}
 
-fs.appendFileSync('fileC', fileA);
-fs.appendFileSync('fileC', fileB);
+concatsFiles('fileA', 'fileB', 'fileC');
 
 // Async >> use Promise to handle
 // fs.readFile('fileA', 'utf8', (err, data) => {
