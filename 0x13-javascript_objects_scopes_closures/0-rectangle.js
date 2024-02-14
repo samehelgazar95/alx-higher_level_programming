ee4e347945f52bd59fb84e4ddd853cefe78f7e35
Sameh ElGazar <selgazar95@gmail.com>
2024-02-14 02:05:50 +0200

    Task 01234

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,2 +1,5 @@
 #!/usr/bin/node
 
+class Rectangle {}
+
+module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+}
+
+module.exports = Rectangle;
