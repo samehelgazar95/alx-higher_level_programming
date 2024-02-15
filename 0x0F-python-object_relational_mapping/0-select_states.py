373bbc00edae33841f30d2e55e899e0cb47d19d3
Sameh ElGazar <selgazar95@gmail.com>
2024-02-16 01:26:29 +0200

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""Print States"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    qry = "SELECT * FROM states ORDER BY states.id ASC"
+
+    db = MySQLdb.connect(
+                        host='localhost',
+                        port=3306,
+                        user=args[0],
+                        password=args[1],
+                        db=args[2],
+                        charset='utf8')
+    cursor = db.cursor()
+    cursor.execute(qry)
+
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+
+    cursor.close()
+    db.close()
