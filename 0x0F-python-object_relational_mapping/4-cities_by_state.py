73d07ce1e2ad9a6e9d02e2bc2dc12190cf73c55b
Sameh ElGazar <selgazar95@gmail.com>
2024-02-16 09:45:01 -0800

    Task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+""" Printing Cities and States """
+import MySQLdb
+import sys
+
+
+def print_cities(myUSER, myPASS, myDB):
+    conn = MySQLdb.connect(
+                host='localhost',
+                port=3306,
+                charset='utf8',
+                passwd=myPASS,
+                user=myUSER,
+                db=myDB
+            )
+
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT cities.id, cities.name, states.name
+        FROM cities INNER JOIN states
+        ON cities.state_id = states.id
+        ORDER BY cities.id ASC;
+    """)
+
+    results = cur.fetchall()
+    for record in results:
+        print(record)
+
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    print_cities(args[1], args[2], args[3])
