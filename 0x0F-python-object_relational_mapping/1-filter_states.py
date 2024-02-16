e68eb58884e3e02fdf8c0a4c291b3993109e47d0
Sameh ElGazar <selgazar95@gmail.com>
2024-02-15 19:07:14 -0800

    Haaah

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""List States Starting with 'N'"""
+import MySQLdb
+import sys
+
+
+def list_state_n(user_name, password, db_name):
+    qry = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         charset='utf8',
+                         user=user_name,
+                         password=password,
+                         db=db_name)
+    curs = db.cursor()
+    curs.execute(qry)
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+
+    curs.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    list_state_n(sys.argv[1], sys.argv[2], sys.argv[3])
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""List States equals to argv[4]"""
+import MySQLdb
+import sys
+
+
+def list_state_n(user_name, password, db_name, state_name):
+    qry = f"SELECT * FROM states\
+           WHERE name = '{state_name}'\
+           ORDER BY states.id ASC"
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         charset='utf8',
+                         user=user_name,
+                         password=password,
+                         db=db_name)
+    curs = db.cursor()
+    curs.execute(qry)
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+
+    curs.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    list_state_n(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
