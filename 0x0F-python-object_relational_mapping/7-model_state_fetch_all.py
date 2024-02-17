d5b7c1ba202911d3c5d2e55e82532fe4f51137e8
Sameh ElGazar <selgazar95@gmail.com>
2024-02-16 19:14:55 -0800

    Task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""
+Printing data from states table
+"""
+import MySQLdb
+import sys
+from model_state import Base, State
+
+
+def list_state(myUser, myPass, myDb):
+    qry = """
+        SELECT * FROM states
+        ORDER BY id ASC
+        """
+
+    with MySQLdb.connect(
+                        user=myUser,
+                        passwd=myPass,
+                        db=myDb,
+                        host='localhost',
+                        port=3306,
+                        charset='utf8') as conn:
+        with conn.cursor() as cur:
+            cur.execute(qry)
+            results = cur.fetchall()
+
+            for res in results:
+                print("{}: {}".format(res[0], res[1]))
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    list_state(args[1], args[2], args[3])
