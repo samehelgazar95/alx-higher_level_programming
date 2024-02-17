b82f93c823ce87a1bf85c8877497ce95c9cbd87d
Sameh ElGazar <selgazar95@gmail.com>
2024-02-17 14:17:16 -0800

    Task 13

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+# coding = 'utf-8'
+"""
+Print all cities in the table
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+
+
+def print_cities(myUser, myPass, myDb):
+    # Create the engine
+    sql_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    full_url = sql_url.format(myUser, myPass, myDb)
+    engine = create_engine(full_url, pool_pre_ping=True)
+
+    # Configure the Session & Create the tables
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+
+    session = Session()
+    results = session.query(State, City).filter(
+            State.id == City.state_id).order_by(City.id)
+
+    for res in results:
+        print("{}: ({} {}".format(
+            res.State.name,
+            res.City.id,
+            res.City.name
+            ))
+
+    session.close()
+
+
+if __name__ == "__main__":
+    print_cities(argv[1], argv[2], argv[3])
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+"""
+Creating a class for cities table
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    City class that inherits from Base
+    And represents cities Table
+
+    Attributes:
+        id: city_id
+        name: city_name
+        state_id: state_id
+        state: state parent
+    """
+    __tablename__ = 'cities'
+
+    id = Column(
+            Integer,
+            autoincrement=True,
+            nullable=False,
+            primary_key=True)
+    name = Column(
+            String(128),
+            nullable=False)
+    state_id = Column(
+            Integer,
+            ForeignKey('states.id'),
+            nullable=False)
