418cac7bea8cb8417d0b0be18b6ec12538c4538c
Sameh ElGazar <selgazar95@gmail.com>
2024-02-16 19:01:03 -0800

    Task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Create declarative_base Class & State Class (Table)"""
+"""
+Create declarative_base Class & State Class (Table)
+"""
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,6 +10,14 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    State class that inherits from Base
+    And represents states Table
+
+    Attributes:
+        id: state_id
+        name: State_name
+    """
     __tablename__ = 'states'
 
     id = Column(Integer,
