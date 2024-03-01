747f7a9267aad5eb8399a757c5ba704b80ec3da9
Sameh ElGazar <selgazar95@gmail.com>
2024-02-29 16:29:46 -0800

    Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Find the peak
+
+
+def find_peak(arr):
+    if len(arr) == 0 or arr is None:
+        return None
+
+    peak = arr[0]
+    n = len(arr)
+
+    for i in range(n):
+        if i != 0 and i != n-1 and arr[i-1] <= arr[i] >= arr[i+1]:
+            if arr[i] > peak:
+                peak = arr[i]
+
+    return peak
