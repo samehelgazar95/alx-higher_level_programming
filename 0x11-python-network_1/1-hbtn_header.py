ae72791f4bad63782e11ae8fe3b900d749457d2c
Sameh ElGazar <selgazar95@gmail.com>
2024-03-02 22:39:11 +0200

    Task2

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Return the X-Request-Id header
+from urllib.request import urlopen
+from sys import argv
+
+
+if argv[1]:
+    with urlopen(argv[1]) as res:
+        print(res.headers['X-Request-Id'])
