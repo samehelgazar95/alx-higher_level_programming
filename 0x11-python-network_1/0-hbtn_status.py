432e1651edd4ff3a6a4d904793b832506dbd33e4
Sameh ElGazar <selgazar95@gmail.com>
2024-03-02 19:22:57 +0200

    Task 0.1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,22 @@
 from urllib.request import urlopen
 
 
-url = "https://alx-intranet.hbtn.io/status"
-with urlopen(url) as res:
-    content = res.read()
-    html = content.decode("UTF-8")
-    msg = """Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}""".format(type(content), content, html)
-    print(msg)
+def url_req(url):
+    """"
+    Requesting url
+    Arguments:
+        url: the url to request
+    """
+    with urlopen(url) as res:
+        content = res.read()
+        html = content.decode("UTF-8")
+        msg = """Body response:
+        - type: {}
+        - content: {}
+        - utf8 content: {}""".format(type(content), content, html)
+        print(msg)
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    url_req(url)
