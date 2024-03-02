47d7d84429ab3261f700622f4ba994a500d8565d
Sameh ElGazar <selgazar95@gmail.com>
2024-03-02 13:18:41 -0800

    Task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-"""  """
-from urllib.request import urlopen
+""" posting email to the url """
+from urllib.request import urlopen, Request
 from urllib.parse import urlencode
 from sys import argv
 
 
+def post_email(url, email):
+    try:
+        params = urlencode({'email': email})
+        params = params.encode('ascii')
+        req = Request(url, params)
+        with urlopen(req) as res:
+            data = res.read()
+            html = data.decode('UTF-8')
+            print(html)
+    except Exception as e:
+        print("An error occured: {}".format(e))
+
+
 if __name__ == '__main__':
-    email = {'email': argv[2]}
-    params = urlencode(email)
-    url = argv[1] + '?' + params
-    with urlopen(url) as res:
-        data = res.read()
-        html = data.decode('UTF-8')
-        print(html)
+    url = argv[1]
+    email = argv[2]
+    post_email(url, email)
+
