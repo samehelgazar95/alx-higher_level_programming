6725b5b3c1d35739933c82c12da701e1623a1c75
Sameh ElGazar <selgazar95@gmail.com>
2024-03-03 00:02:38 +0200

    Task 3.1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,21 +5,10 @@ from urllib.request import Request, urlopen
 from urllib.error import HTTPError
 
 
-def body_or_error(url):
-    """
-    Printing the body or the error code
-    Argument:
-        url: the url to use
-    """
+if __name__ == '__main__':
     try:
-        req = Request(url)
-        with urlopen(req) as res:
-            temp = res.read()
-            data = temp.decode("ascii")
+        with urlopen(argv[1]) as res:
+            data = res.read().decode("UTF-8")
             print(data)
     except HTTPError as e:
         print('Error Code: {}'.format(e.code))
-
-
-if __name__ == '__main__':
-    body_or_error(argv[1])
