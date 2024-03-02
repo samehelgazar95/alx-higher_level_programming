6910a1ce3d4049628388816a847f477e319de9fb
Sameh ElGazar <selgazar95@gmail.com>
2024-03-02 22:46:10 +0200

    Task1.1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,6 +3,6 @@
 from urllib.request import urlopen
 from sys import argv
 
-
-with urlopen(argv[1]) as res:
-    print(res.headers['X-Request-Id'])
+if __name__ == "__main__":
+    with urlopen(argv[1]) as res:
+        print(res.headers['X-Request-Id'])
