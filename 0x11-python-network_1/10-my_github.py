0034ce2875c7aafd5dcab3a424902f33ed146e6e
Sameh ElGazar <selgazar95@gmail.com>
2024-03-03 04:38:30 +0200

    Task 10.0

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,16 +2,17 @@
 """
 Use Github API to display the id
 """
-import requests
+from requests import get
+from requests.auth import HTTPBasicAuth
 from sys import argv
 
 
 def display_id(user, pwd):
     url = 'https://api.github.com/users/{}/{}'.format(user, pwd)
-    req = requests.get(url, auth=(user, pwd))
+    req = get(url, auth=HTTPBasicAuth(user, pwd))
 
     req_json = req.json()
-    print(req_json['id'])
+    print(req_json.get("id"))
 
 
 if __name__ == "__main__":
