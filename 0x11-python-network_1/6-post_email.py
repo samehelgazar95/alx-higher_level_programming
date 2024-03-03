c87b37e511c6c4732e987e13d32582c780db9de8
Sameh ElGazar <selgazar95@gmail.com>
2024-03-02 17:21:59 -0800

    Task 6.1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,5 +5,6 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    res = requests.post(argv[1], params={"email": argv[2]})
+    res = requests.post(argv[1], data={"email": argv[2]})
     print(res.text)
+
