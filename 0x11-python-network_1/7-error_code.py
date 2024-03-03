d3e4d06034d8a158698a06ee6a7e2278e00bb50f
Sameh ElGazar <selgazar95@gmail.com>
2024-03-02 17:28:01 -0800

    Task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Print the status_code if it's > 400 """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    req = requests.get(argv[1])
+    if req.status_code > 400:
+        print("Error code: ", req.status_code)
+    else:
+        print(req.text)
