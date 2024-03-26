d8ddd504a95ef6cb73d611f33e350e34e1b042b8
Sameh ElGazar <selgazar95@gmail.com>
2024-03-27 00:35:39 +0200

    task 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,6 +3,5 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', (err, res) => {
-  if (err) console.log(err);
-  else console.log(res);
+  err ? console.log(err) : console.log(res);
 });
