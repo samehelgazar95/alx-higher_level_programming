d98549bea044961197f3efeb1bf92c16c68da6f1
Sameh ElGazar <selgazar95@gmail.com>
2024-03-27 00:45:49 +0200

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,5 +4,5 @@ const fs = require('fs');
 const args = process.argv;
 
 fs.writeFile(args[2], args[3], 'utf8', (err, res) => {
-  err ? console.log(err) : console.log(res);
+  err && console.log(err);
 });
