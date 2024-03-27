eec91bfa5451c339503f71eccd547a2fecfab0b2
Sameh ElGazar <selgazar95@gmail.com>
2024-03-27 02:24:03 +0200

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,20 +1,14 @@
 #!/usr/bin/node
 
 const request = require('request');
+const fs = require('fs');
 const url = process.argv[2];
-const findMe = 'https://swapi-api.alx-tools.com/api/people/18/';
+const filePath = process.argv[3];
 
 request.get(url, (err, res, body) => {
-  let counter = 0;
   err && console.log(err);
-  const data = JSON.parse(body);
-  const results = data.results;
-
-  results.forEach((e) => {
-    e.characters.forEach((ele) => {
-      if (ele === findMe) counter++;
-    });
+  console.log(body);
+  fs.writeFile(filePath, body, 'utf8', (err) => {
+    err && console.log(err);
   });
-
-  console.log(counter);
 });
