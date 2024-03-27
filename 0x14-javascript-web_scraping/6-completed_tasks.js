a08359408192b16389c8e4866ffe4b1f45cc73c3
Sameh ElGazar <selgazar95@gmail.com>
2024-03-27 02:44:03 +0200

    task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (err, res, body) => {
+  err && console.log(err);
+
+  const data = JSON.parse(body);
+  const completedTasks = {};
+
+  data.forEach((e) => {
+    const completed = e.completed;
+    const userId = e.userId;
+
+    if (completed && !completedTasks[userId]) { completedTasks[userId] = 1; } else { completedTasks[userId]++; }
+  });
+
+  console.log(completedTasks);
+});
